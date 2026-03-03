f2ada7dbd6cbec94fafff9024b9c82edd67eedc2
Allen Ramirez <allenram.15@yahoo.com>
2026-03-03 02:32:42 -0500

    added 12 cards

--- src/App.jsx
+++ src/App.jsx
@@ -10,12 +10,92 @@ function App() {
     <>
       <img className="awning"src="https://heroic-pixie-6d7f91.netlify.app/awning.png"></img>
       <h1>Food Truck Favorites</h1>
-      <Card 
-        img={"https://pyxis.nymag.com/v1/imgs/38c/2ac/b02a093f6b846ebd1aca78b0d59c7e427a-birria-1.rsocial.w1200.jpg"}
-        businessTitle={"Birria-Landia"}
-        type={"Mexican"}
-        menusrc={"https://thebirrialandia.com/menu/"}
-      />
+      <div className="container">
+        {/* Card 1 */}
+        <Card 
+          img={"https://pyxis.nymag.com/v1/imgs/38c/2ac/b02a093f6b846ebd1aca78b0d59c7e427a-birria-1.rsocial.w1200.jpg"}
+          businessTitle={"Birria-Landia"}
+          type={"Mexican"}
+          menusrc={"https://thebirrialandia.com/menu/"}
+        />
+        {/* Card 2 */}
+        <Card 
+          img={"https://pyxis.nymag.com/v1/imgs/38c/2ac/b02a093f6b846ebd1aca78b0d59c7e427a-birria-1.rsocial.w1200.jpg"}
+          businessTitle={"Birria-Landia"}
+          type={"Mexican"}
+          menusrc={"https://thebirrialandia.com/menu/"}
+        />
+        {/* Card 3 */}
+        <Card 
+          img={"https://pyxis.nymag.com/v1/imgs/38c/2ac/b02a093f6b846ebd1aca78b0d59c7e427a-birria-1.rsocial.w1200.jpg"}
+          businessTitle={"Birria-Landia"}
+          type={"Mexican"}
+          menusrc={"https://thebirrialandia.com/menu/"}
+        />
+        {/* Card 4 */}
+        <Card 
+          img={"https://pyxis.nymag.com/v1/imgs/38c/2ac/b02a093f6b846ebd1aca78b0d59c7e427a-birria-1.rsocial.w1200.jpg"}
+          businessTitle={"Birria-Landia"}
+          type={"Mexican"}
+          menusrc={"https://thebirrialandia.com/menu/"}
+        />
+        {/* Card 5 */}
+        <Card 
+          img={"https://pyxis.nymag.com/v1/imgs/38c/2ac/b02a093f6b846ebd1aca78b0d59c7e427a-birria-1.rsocial.w1200.jpg"}
+          businessTitle={"Birria-Landia"}
+          type={"Mexican"}
+          menusrc={"https://thebirrialandia.com/menu/"}
+        />
+        {/* Card 6 */}
+        <Card 
+          img={"https://pyxis.nymag.com/v1/imgs/38c/2ac/b02a093f6b846ebd1aca78b0d59c7e427a-birria-1.rsocial.w1200.jpg"}
+          businessTitle={"Birria-Landia"}
+          type={"Mexican"}
+          menusrc={"https://thebirrialandia.com/menu/"}
+        />
+        {/* Card 7 */}
+        <Card 
+          img={"https://pyxis.nymag.com/v1/imgs/38c/2ac/b02a093f6b846ebd1aca78b0d59c7e427a-birria-1.rsocial.w1200.jpg"}
+          businessTitle={"Birria-Landia"}
+          type={"Mexican"}
+          menusrc={"https://thebirrialandia.com/menu/"}
+        />
+        {/* Card 8 */}
+        <Card 
+          img={"https://pyxis.nymag.com/v1/imgs/38c/2ac/b02a093f6b846ebd1aca78b0d59c7e427a-birria-1.rsocial.w1200.jpg"}
+          businessTitle={"Birria-Landia"}
+          type={"Mexican"}
+          menusrc={"https://thebirrialandia.com/menu/"}
+        />
+        {/* Card 9 */}
+        <Card 
+          img={"https://pyxis.nymag.com/v1/imgs/38c/2ac/b02a093f6b846ebd1aca78b0d59c7e427a-birria-1.rsocial.w1200.jpg"}
+          businessTitle={"Birria-Landia"}
+          type={"Mexican"}
+          menusrc={"https://thebirrialandia.com/menu/"}
+        />
+        {/* Card 10 */}
+        <Card 
+          img={"https://pyxis.nymag.com/v1/imgs/38c/2ac/b02a093f6b846ebd1aca78b0d59c7e427a-birria-1.rsocial.w1200.jpg"}
+          businessTitle={"Birria-Landia"}
+          type={"Mexican"}
+          menusrc={"https://thebirrialandia.com/menu/"}
+        />
+        {/* Card 11 */}
+        <Card 
+          img={"https://pyxis.nymag.com/v1/imgs/38c/2ac/b02a093f6b846ebd1aca78b0d59c7e427a-birria-1.rsocial.w1200.jpg"}
+          businessTitle={"Birria-Landia"}
+          type={"Mexican"}
+          menusrc={"https://thebirrialandia.com/menu/"}
+        />
+        {/* Card 12 */}
+        <Card 
+          img={"https://pyxis.nymag.com/v1/imgs/38c/2ac/b02a093f6b846ebd1aca78b0d59c7e427a-birria-1.rsocial.w1200.jpg"}
+          businessTitle={"Birria-Landia"}
+          type={"Mexican"}
+          menusrc={"https://thebirrialandia.com/menu/"}
+        />
+      </div>
     </>
   )
 }
