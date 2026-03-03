bb44950863e0fd27c444bb778430468151542c1f
Allen Ramirez <allenram.15@yahoo.com>
2026-03-03 02:53:29 -0500

    completed project

--- src/App.jsx
+++ src/App.jsx
@@ -20,80 +20,80 @@ function App() {
         />
         {/* Card 2 */}
         <Card 
-          img={"https://pyxis.nymag.com/v1/imgs/38c/2ac/b02a093f6b846ebd1aca78b0d59c7e427a-birria-1.rsocial.w1200.jpg"}
-          businessTitle={"Birria-Landia"}
-          type={"Mexican"}
-          menusrc={"https://thebirrialandia.com/menu/"}
+          img={"https://res.cloudinary.com/the-infatuation/image/upload/c_fill,w_1200,ar_4:3,g_auto,f_auto/cms/reviews/mysttik-masala/banners/1561989798.42"}
+          businessTitle={"Mysttik Masala"}
+          type={"Indian"}
+          menusrc={"https://indianfoodny.com/"}
         />
         {/* Card 3 */}
         <Card 
-          img={"https://pyxis.nymag.com/v1/imgs/38c/2ac/b02a093f6b846ebd1aca78b0d59c7e427a-birria-1.rsocial.w1200.jpg"}
-          businessTitle={"Birria-Landia"}
-          type={"Mexican"}
-          menusrc={"https://thebirrialandia.com/menu/"}
+          img={"https://nypost.com/wp-content/uploads/sites/2/2014/08/081114featureshalaguysbz-1.jpg?quality=75&strip=all&w=1024"}
+          businessTitle={"The Halal Guys"}
+          type={"Middle Eastern"}
+          menusrc={"https://thehalalguys.com/menu/"}
         />
         {/* Card 4 */}
         <Card 
-          img={"https://pyxis.nymag.com/v1/imgs/38c/2ac/b02a093f6b846ebd1aca78b0d59c7e427a-birria-1.rsocial.w1200.jpg"}
-          businessTitle={"Birria-Landia"}
-          type={"Mexican"}
-          menusrc={"https://thebirrialandia.com/menu/"}
+          img={"https://nydosas.com/wp-content/uploads/2018/07/maxresdefault-2-1024x630.jpg"}
+          businessTitle={"NY Dosas"}
+          type={"Vegetarian"}
+          menusrc={"https://nydosas.com/menu/"}
         />
         {/* Card 5 */}
         <Card 
-          img={"https://pyxis.nymag.com/v1/imgs/38c/2ac/b02a093f6b846ebd1aca78b0d59c7e427a-birria-1.rsocial.w1200.jpg"}
-          businessTitle={"Birria-Landia"}
-          type={"Mexican"}
-          menusrc={"https://thebirrialandia.com/menu/"}
+          img={"https://infatuation.imgix.net/media/images/reviews/jerk-pan/banners/1561989308.8.jpg"}
+          businessTitle={"Jerk Pan"}
+          type={"Jamaican"}
+          menusrc={"https://www.allmenus.com/ny/new-york/360888-jerk-pan/menu/"}
         />
         {/* Card 6 */}
         <Card 
-          img={"https://pyxis.nymag.com/v1/imgs/38c/2ac/b02a093f6b846ebd1aca78b0d59c7e427a-birria-1.rsocial.w1200.jpg"}
-          businessTitle={"Birria-Landia"}
-          type={"Mexican"}
-          menusrc={"https://thebirrialandia.com/menu/"}
+          img={"https://static01.nyt.com/images/2019/09/04/dining/29hungry-tong2/merlin_159654336_783be212-7726-485e-9250-4793a743af41-jumbo.jpg"}
+          businessTitle={"Tong"}
+          type={"Bangladeshi"}
+          menusrc={"https://www.facebook.com/Bangladeshistreetfoods/"}
         />
         {/* Card 7 */}
         <Card 
-          img={"https://pyxis.nymag.com/v1/imgs/38c/2ac/b02a093f6b846ebd1aca78b0d59c7e427a-birria-1.rsocial.w1200.jpg"}
-          businessTitle={"Birria-Landia"}
-          type={"Mexican"}
-          menusrc={"https://thebirrialandia.com/menu/"}
+          img={"https://kingsouvlakinyc.com/wp-content/themes/king/images/location-f.jpg"}
+          businessTitle={"King Souvlaki of Astoria"}
+          type={"Greek"}
+          menusrc={"https://kingsouvlakinyc.com/"}
         />
         {/* Card 8 */}
         <Card 
-          img={"https://pyxis.nymag.com/v1/imgs/38c/2ac/b02a093f6b846ebd1aca78b0d59c7e427a-birria-1.rsocial.w1200.jpg"}
-          businessTitle={"Birria-Landia"}
-          type={"Mexican"}
-          menusrc={"https://thebirrialandia.com/menu/"}
+          img={"https://fastly.4sqi.net/img/general/600x600/61129566_7PLEKHNMZ-VNqzYx2jWRVZGrZ42PdPX0M-j9Ftpg8Ts.jpg"}
+          businessTitle={"LIng's Sweet Mini Cakes"}
+          type={"Chinese"}
+          menusrc={"https://canalstreet.market/"}
         />
         {/* Card 9 */}
         <Card 
-          img={"https://pyxis.nymag.com/v1/imgs/38c/2ac/b02a093f6b846ebd1aca78b0d59c7e427a-birria-1.rsocial.w1200.jpg"}
-          businessTitle={"Birria-Landia"}
-          type={"Mexican"}
-          menusrc={"https://thebirrialandia.com/menu/"}
+          img={"https://d3hbe0kmbam4a5.cloudfront.net/photos/67e1cdb7-bc27-4f26-8343-851bfaa7fc22.jpg"}
+          businessTitle={"Uncle Gussy's"}
+          type={"Greek"}
+          menusrc={"https://unclegussys.com/menus/"}
         />
         {/* Card 10 */}
         <Card 
-          img={"https://pyxis.nymag.com/v1/imgs/38c/2ac/b02a093f6b846ebd1aca78b0d59c7e427a-birria-1.rsocial.w1200.jpg"}
-          businessTitle={"Birria-Landia"}
-          type={"Mexican"}
-          menusrc={"https://thebirrialandia.com/menu/"}
+          img={"https://live.staticflickr.com/8088/8400188739_53b16c7d13_z.jpg"}
+          businessTitle={"Patacon Pisao"}
+          type={"Venezuelan"}
+          menusrc={"https://www.pataconpisaonyc.com/menus/"}
         />
         {/* Card 11 */}
         <Card 
-          img={"https://pyxis.nymag.com/v1/imgs/38c/2ac/b02a093f6b846ebd1aca78b0d59c7e427a-birria-1.rsocial.w1200.jpg"}
-          businessTitle={"Birria-Landia"}
-          type={"Mexican"}
-          menusrc={"https://thebirrialandia.com/menu/"}
+          img={"https://infatuation.imgix.net/media/images/reviews/moms-momo/banners/1606245507.888788.png"}
+          businessTitle={"Mom's Mono"}
+          type={"Tibetan"}
+          menusrc={"https://www.restaurantji.com/ny/jackson-heights/moms-momo-/menu/"}
         />
         {/* Card 12 */}
         <Card 
-          img={"https://pyxis.nymag.com/v1/imgs/38c/2ac/b02a093f6b846ebd1aca78b0d59c7e427a-birria-1.rsocial.w1200.jpg"}
-          businessTitle={"Birria-Landia"}
-          type={"Mexican"}
-          menusrc={"https://thebirrialandia.com/menu/"}
+          img={"https://fastly.4sqi.net/img/general/600x600/784042_29bZZHPGisHnWqyIuuXMo8D2zzW7QS6Hft7Vv15SH_I.jpg"}
+          businessTitle={"Makina Cafe"}
+          type={"Ethiopian"}
+          menusrc={"https://makinacafenyc.com/"}
         />
       </div>
     </>
@@ -106,7 +106,7 @@ function Card({img, businessTitle, type, menusrc}){
       <img src={img}></img>
       <h3>{businessTitle}</h3>
       <h4>{type}</h4>
-      <a href={menusrc}>View Menu</a>
+      <a class="menu" href={menusrc}>View Menu</a>
     </div>
   )
 }
